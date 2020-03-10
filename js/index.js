5bc0fe312ea095a079280bc45bfa884812c8c1d2
Marissa <marissacooterl@gmail.com>
2020-03-10 16:24:50 -0500

    Finished using ten different event listeners. Moving on to task two.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+// EXAMPLE:
+let logo = document.querySelector("h1.logo-heading");
+logo.addEventListener("dblclick", event => event.target.style.color = "lightblue");
+
+let bodyColor = document.querySelector("body");
+bodyColor.addEventListener("keyup", event => event.target.style.backgroundColor = "#aaf0d1");
+
+let headerColor = document.querySelector("header");
+headerColor.addEventListener("mouseover", event => event.target.style.backgroundColor = "#C8A2C8");
+
+let headerimg = document.querySelector(".intro img");
+headerimg.addEventListener("drag", event => {
+    headerimg.setAttribute("src", "img/hello-kitty-credit-sanrio.jpg")
+});
+
+let footerColor = document.querySelector("footer");
+footerColor.addEventListener("click", event => event.target.style.backgroundColor = "hotpink");
+
+let bodyTitle = document.querySelector("h2.body-title");
+bodyTitle.addEventListener("mouseleave", event => event.target.style.color = "hotpink" );
+
+let btnColor = document.querySelector(".btn");
+btnColor.addEventListener("mouseenter", event => event.target.style.backgroundColor = "hotpink");
+
+let firstimg = document.querySelector(".img-content img");
+firstimg.addEventListener("mousemove", event => {
+    firstimg.setAttribute("src", "img/hk.jpeg")
+});
+
+// fix this later, new img is popping up over first edited img
+let secondimg = document.querySelector(".secondimg-content img");
+secondimg.addEventListener("mouseup", event => {
+    firstimg.setAttribute("src", "img/bow.jpg")
+});
+
+let dest = document.querySelectorAll("div.destination p");
+dest.forEach(element => {
+    element.addEventListener("dblclick", event => event.target.style.color = "hotpink"
+)});
+
+
+
